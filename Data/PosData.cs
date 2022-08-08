19372afe928ac063a97e3dd1b3e2d5c0ee9685e1
Александр Воскобойник <magical.blade@gmail.com>
2022-08-08 20:43:59 +0300

    Добавьте файлы проекта.

---
+++ Data/PosData.cs
@@ -0,0 +1,72 @@
+﻿using KompasAPI7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositionApplicability.Data
+{
+    internal class PosData
+    {
+        /// <summary>
+        /// Наименование марки
+        /// </summary>
+        public string? Mark { get => _mark; set => _mark = value; }
+        private string? _mark;
+        /// <summary>
+        /// Номер позиции
+        /// </summary>
+        public string? Pos { get => _pos; set => _pos = value; }
+        private string? _pos;
+        /// <summary>
+        /// Количество позиций
+        /// </summary>
+        public string? Quantity { get => _quantity; set => _quantity = value; }
+        private string? _quantity;
+        /// <summary>
+        /// Сечение позиции (толщина х ширина)
+        /// </summary>
+        public string? Size { get => _size; set => _size = value; }
+        private string? _size;
+        /// <summary>
+        /// Длина детали
+        /// </summary>
+        public string? Leigth { get => _leigth; set => _leigth = value; }
+        private string? _leigth;
+        /// <summary>
+        /// Марка стали позиции
+        /// </summary>
+        public string? Steel { get => steel; set => steel = value; }
+        private string? steel;
+
+        /// <summary>
+        /// Масса позиции
+        /// </summary>
+        public string? Weight { get => _weight; set => _weight = value; }
+        private string? _weight;
+        /// <summary>
+        /// Общая масса позиции
+        /// </summary>
+        public string? TotalMass { get => _totalMass; set => _totalMass = value; }
+        private string? _totalMass;
+        /// <summary>
+        /// Номер листа чертежа
+        /// </summary>
+        public string? List { get => _list; set => _list = value; }
+        private string? _list;
+
+        public PosData(ITable table, int row, string nameFile)
+        {
+            Mark = nameFile;
+            Pos = ((IText)table.Cell[row, 0].Text).Str;
+            Quantity = ((IText)table.Cell[row, 1].Text).Str;
+            Size = ((IText)table.Cell[row, 3].Text).Str;
+            Leigth = ((IText)table.Cell[row, 4].Text).Str;
+            Steel = ((IText)table.Cell[row, 5].Text).Str;
+            Weight = ((IText)table.Cell[row, 6].Text).Str;
+            TotalMass = ((IText)table.Cell[row, 7].Text).Str;
+            List = ((IText)table.Cell[row, 8].Text).Str;
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+using Kompas6API5;
+using KompasAPI7;
+using Kompas6Constants;
+using System.Runtime.InteropServices;
+using PositionApplicability.Data;
+using Kompas6Constants3D;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PositionApplicability.ViewModels
+{
+    internal partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string? _pathFolder;
+
+        [ObservableProperty]
+        private List<PosData> _posList = new();
+
+        [ICommand]
+        private void Submit()
+        {
+            Type? kompasType = Type.GetTypeFromProgID("Kompas.Application.5", true);
+            if (kompasType == null) return;
+            KompasObject? kompas = Activator.CreateInstance(kompasType) as KompasObject; //Запуск компаса
+            if (kompas == null) return;
+            IApplication application = (IApplication)kompas.ksGetApplication7();
+            IDocuments documents = application.Documents;
+            IKompasDocument2D kompasDocuments2D = (IKompasDocument2D)documents.Open("d:\\Работа\\Хохлы\\КМД\\Блок К1т\\Блок К1т.cdw", false, false);
+            IViewsAndLayersManager viewsAndLayersManager = kompasDocuments2D.ViewsAndLayersManager;
+            IViews views = viewsAndLayersManager.Views;
+
+            foreach (IView item in views)
+            {
+                ISymbols2DContainer symbols2DContainer = (ISymbols2DContainer)item;
+                IDrawingTables drawingTables = symbols2DContainer.DrawingTables;
+                foreach (ITable table in drawingTables)
+                {
+                    IText text = (IText)table.Cell[0,0].Text;
+                    if (text.Str.IndexOf("Спецификация") != -1 && table.RowsCount > 2 && table.ColumnsCount == 9)
+                    {
+                        for (int row = 3; row < table.RowsCount; row++)
+                        {
+                            PosList.Add(new PosData(table, row, kompasDocuments2D.Name));
+                        }
+                    }
+                }
+            }
+            PosList.Sort();
+            kompas.Quit();
+        }
+
+        [ICommand]
+        private void OpenFolderDialog()
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                PathFolder = dialog.SelectedPath;
+            }
+        }
+    }
+}
